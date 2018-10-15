740ba37a23c051f18cf4351139558bb592451296
MasterEldan <eldankahrovic2208@gmail.com>
2018-10-15 17:22:36 +0200

    Lekcija 10

--- app/src/main/java/org/zenmasters/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/org/zenmasters/swoosh/Controller/SkillActivity.kt
@@ -2,6 +2,7 @@ package org.zenmasters.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_skill.*
@@ -14,11 +15,23 @@ class SkillActivity : BaseActivity() {
 
     lateinit var player : Player
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
 
         player = intent.getParcelableExtra(EXTRA_PLAYER)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun onBallerClicked(view: View){
